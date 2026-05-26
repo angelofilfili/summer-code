2b8f3a2e462335339089dcb24f348614c3012bc1
Angelo <Angelofilfili0@gmail.com>
2026-05-26 15:11:26 -0400

    Using weather api key to make weather lookup script

---
+++ week3learning/OpenWeatherMap.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+
+while True:
+
+    city_name = input("Name a city (enter Q to quit): ")
+
+    if city_name == "Q":
+        break
+
+    try:
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=imperial")
+        if response.status_code == 404:
+            raise Exception()
+        data = response.json()
+        print("Temperature:", data["main"]["temp"], "F")
+        print("Humidity:", data["main"]["humidity"], "%")
+
+
+        description = data["weather"][0]["description"]
+        print("Weather Description:", description)
+
+    except:
+        print("City doesn't exist!")
+        print("Try again!")
+
+
+
+
+                            
+
